430a36a2c690a958c6d7c3699dcde2016ea4154a
Ryoung27 <RRYoung89@gmail.com>
2018-09-28 14:39:45 -0500

    added extension

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Richard Young on 9/28/18.
+//  Copyright © 2018 Richard Young. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
+                    // do stuff!
+                }
+            }
+        }
+    }
+}
