87b2b849bd6f79a9929c0ef283d0be3be07ab8c2
Ryoung27 <RRYoung89@gmail.com>
2018-09-28 15:15:07 -0500

    finished project

--- Extension/Action.js
+++ Extension/Action.js
@@ -15,7 +15,7 @@ var Action = function() {};
 Action.prototype = {
     
 run: function(parameters) {
-    
+    parameters.completionFunction({"URL": document.URL, "title": document.title });
 },
     
 finalize: function(parameters) {
